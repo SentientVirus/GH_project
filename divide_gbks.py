f06396018f9305e33c16087b59eb8d0d81e82f38
Marina <marina.motamerlo@icm.uu.se>
2022-11-29 13:53:23 +0100

    Update script so that it divides gbks into several records

--- divide_gbks.py
+++ divide_gbks.py
@@ -5,12 +5,25 @@ Created on Tue Nov 29 10:01:24 2022
 
 @author: marina
 """
-
-from Bio import SeqIO
 import os
 
-directory = 'analyses/data/genbanks'
+indir = 'data/genbanks'
+outdir = 'gbks'
+newfile = {}
 
-for file in os.listdir(directory):
-    for record in SeqIO.parse(file, 'genbank'):
-        print(record.id)
+for file in sorted(os.listdir(indir)):
+    if file.endswith('.gbk'):
+        with open(f'{indir}/{file}', 'r') as handle:
+            f = (line for line in handle)
+            # for record in GenBank.parse(handle):
+            #     print(record)
+            for line in f:
+                if 'LOCUS' in line:
+                    locus_name = line.split()[1]
+                    newfile[locus_name] = ''
+                    print(locus_name)
+                newfile[locus_name] += line
+                
+for locus_name in newfile.keys():
+    with open(f'{outdir}/{locus_name}', 'w') as outgbk:
+        outgbk.write(newfile[locus_name])
