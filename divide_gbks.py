4175b1c0705098e97d4f06fe420d8b9d234fff23
Marina <marina.motamerlo@icm.uu.se>
2022-11-30 17:51:47 +0100

    Update divide_gbks.py with functions and comments

--- divide_gbks.py
+++ divide_gbks.py
@@ -5,25 +5,35 @@ Created on Tue Nov 29 10:01:24 2022
 
 @author: marina
 """
+# =============================================================================
+# Script to divide multi-record GenBank files into single-record files.
+# =============================================================================
+
 import os
 
 indir = 'data/genbanks'
 outdir = 'gbks'
 newfile = {}
 
-for file in sorted(os.listdir(indir)):
+#Function to parse GenBank files, it outputs record names and record info
+def parse_GenBank(file):
     if file.endswith('.gbk'):
-        with open(f'{indir}/{file}', 'r') as handle:
+        with open(file, 'r') as handle:
             f = (line for line in handle)
-            # for record in GenBank.parse(handle):
-            #     print(record)
             for line in f:
                 if 'LOCUS' in line:
                     locus_name = line.split()[1]
-                    newfile[locus_name] = ''
+                    newfile = ''
                     print(locus_name)
-                newfile[locus_name] += line
-                
+                newfile += line
+    return locus_name, newfile
+
+#Results are saved into a dictionary
+for file in sorted(os.listdir(indir)):
+    locus_name, new_file = parse_GenBank(f'{indir}/{file}')
+    newfile[locus_name] = new_file
+
+#Individual records are saved in separate files                
 for locus_name in newfile.keys():
     with open(f'{outdir}/{locus_name}.gbk', 'w') as outgbk:
         outgbk.write(newfile[locus_name])
