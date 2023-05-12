fa23131140e9dc4be13ce6d115c7e5e373fb3e8f
Marina <marina.motamerlo@icm.uu.se>
2023-05-12 16:01:57 +0200

    Generate Interproscan annotations for the new strains

---
+++ change_ids.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 11:45:45 2023
+
+@author: marina
+"""
+
+# =============================================================================
+# Script to change protein IDs in Interproscan annotations to locus tags
+# =============================================================================
+
+import pandas as pd
+from Bio import SeqIO
+
+path = "../Akunkeei_files/gbff"
+new_strains = ["DSMZ12361", "IBH001"]
+prot2loctag = {}
+for s in new_strains:
+    with open(f"{path}/{s}_genomic.gbff") as genb:
+        info = SeqIO.parse(genb, "genbank")
+        for record in info:
+            for feature in record.features:
+                tag = feature.qualifiers.get('locus_tag')
+                prot_id = feature.qualifiers.get('protein_id')
+                if tag and prot_id:
+                    tag = tag[0]
+                    prot_id = prot_id[0]
+                    prot2loctag[prot_id] = tag
+    with open(f"{s}_id.tsv") as df_file:
+        df = pd.read_csv(df_file, sep = "\t", header = None)
+        protein_ids = df[0]
+        df2 = df.replace({0: prot2loctag})
+        df2.to_csv(f"{s}.tsv", sep = "\t", header = False, index = False)
