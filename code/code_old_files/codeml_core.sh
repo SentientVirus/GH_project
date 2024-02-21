c3800ccde6421da1e0a5795ce3b2691fe0b60d07
Marina <marina.motamerlo@icm.uu.se>
2024-02-21 17:55:03 +0100

    Removed unused files

---
+++ code/code_old_files/codeml_core.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This file was originally created to trim the set of core genes, but it wasn't necessary in later steps.
+
+input_dir="untrimmed_core"
+output_dir="trimmed_core"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+# Loop through files ending with genes.fasta in the input directory
+for file in "$input_dir"/*genes.fasta; do
+    filename=$(basename "$file")
+    output_file="$output_dir/$filename"
+    
+    # Trim the file using Trimal with desired options
+    trimal -in "$file" -out "$output_file" -gappyout
+
+    echo "Trimmed file: ${output_file}"
+done
