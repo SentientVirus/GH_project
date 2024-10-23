51d208d69c5f02f7484fcf0464d35d765a374712
Marina <marina.motamerlo@icm.uu.se>
2024-10-23 15:02:06 +0200

    Update formatting of matrices

--- code/13-matrix_plot.py
+++ code/13-matrix_plot.py
@@ -16,10 +16,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import matplotlib as mpl
+from matplotlib.transforms import ScaledTranslation
 
 workdir = os.path.expanduser('~') + '/GH_project'
 outdir = f'{workdir}/plots/heatmaps'
 genes = ['GS1', 'GS2', 'BRS', 'NGB']
+outplot = f'{outdir}/GH70_dNdS_matrix.svg'
 
 gene_no = {'GS1':  33, 'GS2': 14, 'BRS': 17, 'NGB': 27}
 
@@ -31,7 +33,36 @@ replace_str = {'K2W83_RS': 'DSM_', 'APS55_RS': 'MP2_', 'LDX55_': 'IBH001_'}
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+fig_width = 34
+fig_height = 30
+
+fig, ax = plt.subplot_mosaic([['A', 'B', ''], ['C', 'D', '']], 
+                             figsize=(fig_width, fig_height),
+                             gridspec_kw={'width_ratios': [14, 14, 1]})
+
+plt.style.use('seaborn-pastel')
+plt.subplots_adjust(hspace = 0.4, wspace = 0.1)
+
+cmap = mpl.cm.magma
+norm = mpl.colors.Normalize(vmin = 0, vmax = 1.5)
+
+colbar = fig.colorbar(mpl.cm.ScalarMappable(norm = norm, cmap = cmap),
+             cax = ax[''], orientation = 'vertical', 
+             ticks = np.arange(0, 1.60, 0.1))
+ax[''].yaxis.set_label_position('left')
+
+colbar.ax.tick_params(labelsize = 24)
+colbar.set_label(r'$d_N$ and $d_S$ value', size = 24)
+colbar.outline.set_linewidth(4)
+
 for gene in genes:
+    if gene == 'GS1':
+        axn = 'A'
+    elif gene == 'GS2':
+        axn = 'B'
+    elif gene == 'BRS':
+        axn = 'C'
+    else: axn = 'D'
     file = f'{workdir}/results/{gene}/dNdS.tsv'
     with open(file) as comp_file:
         file_info = pd.read_csv(comp_file, sep = '\t')
@@ -50,34 +81,31 @@ for gene in genes:
         plot_matrix[n, m] = row['dS']
         plot_matrix[m, n] = row['dN']
     plot_matrix[plot_matrix > 1.5] = 1.5
-    plt.style.use('seaborn-pastel')
     
     num_rows, num_cols = plot_matrix.shape
-    fig_width = 18
-    fig_height = 18
-    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
+    for axis in ['top','bottom','left','right']:
+        ax[axn].spines[axis].set_linewidth(4)
 
-    plt.imshow(plot_matrix, vmin=vmin, vmax=vmax, cmap='magma')
-    row_indices = ['Row 1', 'Row 2', 'Row 3']
-    col_indices = ['Col 1', 'Col 2', 'Col 3']
-    plt.xticks(np.arange(len(unique_loci)), unique_loci, rotation = 90, fontsize = 16)
-    plt.yticks(np.arange(len(unique_loci)), unique_loci, fontsize = 16)
-    plt.xlabel('Locus tag 1', size = 24)
-    plt.ylabel('Locus tag 2', size = 24)
-    colbar = plt.colorbar(ticks = np.arange(0, 1.75, 0.10),  shrink = 0.8)
-    colbar.ax.tick_params(labelsize = 16)
-    colbar.set_label(r'$d_N$ and $d_S$ value', size = 24)
-    colbar.outline.set_visible(False)
-    fig.frameon = True
+    ax[axn].imshow(plot_matrix, vmin=vmin, vmax=vmax, cmap='magma')
+
+    ax[axn].set_xticks(np.arange(len(unique_loci)))
+    ax[axn].set_xticklabels(unique_loci, rotation = 90, fontsize = 16)
+    ax[axn].set_yticks(np.arange(len(unique_loci)))
+    ax[axn].set_yticklabels(unique_loci, fontsize = 16)
+    ax[axn].set_xlabel('Locus tag 1', size = 24)
+    ax[axn].set_ylabel('Locus tag 2', size = 24)
+    ax[axn].set_title(gene, size = 30)
+    ax[axn].text(0, 1.1, axn, transform=(
+           ax[axn].transAxes + ScaledTranslation(-20/72, +7/72, fig.dpi_scale_trans)),
+       fontsize = 36, va = 'top', ha = 'left', bbox = dict(facecolor='none', 
+                                                           edgecolor='black', 
+                                                           pad = 20))
+
 
     mpl.rcParams.update(mpl.rcParamsDefault)
 
-    plt.savefig(f'{outdir}/{gene}_dNdS_matrix.svg', format = 'svg')
-    plt.savefig(f'{outdir}/{gene}_dNdS_matrix.png', format = 'png')
-    plt.savefig(f'{outdir}/{gene}_dNdS_matrix.tiff', format = 'tiff')
-    plt.show()
+plt.savefig(outplot, format = 'svg')
+plt.savefig(outplot.replace('svg', 'png'), format = 'png')
+plt.savefig(outplot.replace('svg', 'tiff'), format = 'tiff')
+plt.show()
         
