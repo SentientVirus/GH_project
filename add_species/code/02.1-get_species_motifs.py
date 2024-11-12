13994dc75010c294e6b3b5cff56c3f69f66ce3cc
Marina <marina.motamerlo@icm.uu.se>
2024-11-12 18:40:12 +0100

    Comment script

--- add_species/code/02.1-get_species_motifs.py
+++ add_species/code/02.1-get_species_motifs.py
@@ -9,15 +9,19 @@ alignment is not needed as input.
 
 @author: Marina Mota-Merlo
 """
+
+# =============================================================================
+# 0. Import required packages
+# =============================================================================
+
 import re
 import os
 from Bio import SeqIO
 
-GH_types = ['GS1', 'GS2', 'BRS', 'NGB']
-
 # =============================================================================
 # 0. Object class definition
 # =============================================================================
+
 class motif:
     def __init__(self, num, seq, start, end): #Define motif class
         self.num = num #Motif number (1-7)
@@ -30,135 +34,133 @@ class motif:
 class prot_sequence: #All motifs associated to protein (1 sequence/position per motif)
     def __init__(self, locus, I, II, III, IV, V, VI, VII):
         self.locus = locus #Locus tag
-        self.I = I #Domain 1
-        self.II = II #Domain 2
+        self.I = I #Domain 1 [[1, seq, start, end]]
+        self.II = II #Domain 2 [[2, seq, start, end]]
         self.III = III
         self.IV = IV
         self.V = V
         self.VI = VI
-        self.VII = VII #Domain 7
+        self.VII = VII #Domain 7 [[7, seq, start, end]]
         self.n_motifs = 0 #Number of motifs
-        self.bool_list = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.bool_list = [0, 0, 0, 0, 0, 0, 0, 0] #List to store motif presence/absence
     def calculate_n_motifs(self): #Function to calculate number of motifs in the protein
-        motif_list = [self.I, self.II, self.III, self.IV, self.V, self.VI, self.VII]
-        bool_list = [0 + len(motif) for motif in motif_list]
-        self.bool_list = [0] + bool_list
-        self.n_motifs = sum(bool_list)
+        motif_list = [self.I, self.II, self.III, self.IV, self.V, self.VI, self.VII] #List of lists of lists with motif information
+        bool_list = [0 + len(motif) for motif in motif_list] #Add the length (always 1) of each list with motif information to the bool_list
+        self.bool_list = [0] + bool_list #Add a 0 in the beginning so that indexes correspond to motif numbers
+        self.n_motifs = sum(bool_list) #Calculate the overall number of motifs
     def __str__(self): #What print(class) returns
         return f'<{self.locus} with {self.n_motifs} motifs>'
     
 # =============================================================================
 # 0. Function definition    
 # =============================================================================
-# Adjust this function to make sure that all the motifs are identified
+
 def search_motif(seq_str, num):
     '''Function to search for any motifs by using string patterns
     Input: Amino acid sequence
     Output: Motifs'''
+    
+    #Regular expressions for each motif
     motif_list = ['', r'[MNAVTE]D(.{1,1})V(.{1,1})[ND][QL]', r'(.{1,1})R(.{1,1})DA(.{2,2})[DNFSH][MVIYL][DHNS]',
                   r'[HY][VLI][QSHNATV][LIY][LVNI]E(.*?)(([WGPS](.{3,3})[DGTSEV])|(SLEAAT))',
                   r'[FIY][MVSIL][TRHN][SNAV]HD(.{1,2})[RAVISEP][NKQ](.{2,2})[IVL]',
                   r'[EDA][FLM][LV][LVI][AG][VNMSD][DQ][LIVE][AD]N[SQ]N[PVT]', 
                   r'[LW]G[IVF](.{3,3})[EQW][FLM][AP][PG][HQAS]Y',
-                  r'[MVTS][VTI][PT][TRQ][VMITL][YF]YGD']
+                  r'[MVTS][VTI][PT][TRQ][VMITL][YF]YGD'] 
     
-    motifs = []
-
-    motif_info = re.finditer(motif_list[num], seq_str)
-    for motif_match in motif_info:
+    motifs = [] #Create empty list to store the motifs
+    motif_info = re.finditer(motif_list[num], seq_str) #Find all the possible matches to the motif in the protein
+    
+    for motif_match in motif_info: #Loop through the matches
         if motif_match is not None: #If there is a match, store new values
-            motif_seq = motif_match.group(0)
-            start = motif_match.start() + 1
-            end = motif_match.end() + 1
+            motif_seq = motif_match.group(0) #Convert the match object to a string with the sequence
+            start = motif_match.start() + 1 #Set the motif start (index starts at 1)
+            end = motif_match.end() + 1 #Set the motif end
         else: #Otherwise use generic values
-            motif_seq = ''
-            start = 0
+            motif_seq = '' #Empty sequence
+            start = 0 
             end = 0
-        motifs.append([num, motif_seq, start, end])
+        motifs.append([num, motif_seq, start, end]) #Add list with motif information to the motifs list
     return motifs
 
 # =============================================================================
 # 0. Input definitions
 # =============================================================================
-# infolder = '../data/codons'
-infile1 = os.path.expanduser('~') + '/GH_project/add_species/files/ingroups.faa' #'/GH_project/add_species/results/domains/ingroup_domains.fasta'
-infile2 = os.path.expanduser('~') + '/GH_project/add_species/files/other_outgroups.faa'
-outdir = os.path.expanduser('~') + '/GH_project/add_species/results/motifs'
-output_file = 'other_species_GH70_motif_presence.tsv' #'GH70_motif_presence.tsv' #'motif_presence.tsv'
-output_file2 = 'other_species_GH70_motifs.tsv' #'GH70_motifs.tsv' #'gtf_motifs.tsv'
-prot_list = []
+
+infile1 = os.path.expanduser('~') + '/GH_project/add_species/files/ingroups.faa' #Fasta file with full-protein sequences
+infile2 = os.path.expanduser('~') + '/GH_project/add_species/files/other_outgroups.faa' #Second file with extra sequences
+outdir = os.path.expanduser('~') + '/GH_project/add_species/results/motifs' #Output directory
+output_file = 'other_species_GH70_motif_presence.tsv' #Output file with motif presence/absence
+output_file2 = 'other_species_GH70_motifs.tsv' #Output file with motif sequences and positions
+GH_types = ['GS1', 'GS2', 'BRS', 'NGB'] #Types of genes to be considered
 
 # =============================================================================
 # 1. Create output files and directories
 # =============================================================================
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
+if not os.path.exists(outdir): #If the output directory does not exist
+    os.makedirs(outdir) #Create it
 
-with open(f'{outdir}/{output_file}', 'w') as out_file, open(f'{outdir}/{output_file2}', 'w') as out_file2:
-    out_file.write('locus\ttype\tmotif_I\tmotif_II\tmotif_III\tmotif_IV\tmotif_V\tmotif_VI\tmotif_VII\n')
-    out_file2.write('locus\ttype\tmotif_type\tsequence\tstart\tend\ttotal_length\n')
+with open(f'{outdir}/{output_file}', 'w') as out_file, open(f'{outdir}/{output_file2}', 'w') as out_file2: #Create or overwrite output files
+    out_file.write('locus\ttype\tmotif_I\tmotif_II\tmotif_III\tmotif_IV\tmotif_V\tmotif_VI\tmotif_VII\n') #Add headers to the presence/absence file
+    out_file2.write('locus\ttype\tmotif_type\tsequence\tstart\tend\ttotal_length\n') #Add header to the file with motif sequences and positions
 
 # =============================================================================
 # 2. Assign a type to the sequence if possible
 # =============================================================================
-# OBS! Modify tag assignment for a single input file
-tag_type = {}
-for infile in [infile1, infile2]:
-    with open(infile) as handle:
-        aa_seqs = SeqIO.parse(handle, 'fasta')
-        for fasta in aa_seqs:
-            print(fasta.id)
-            if ('Dsr' in fasta.id or 'falk' in fasta.id) and not fasta.id.endswith('_2'):
-                tag_type[fasta.id] = 'DSR'
-            elif fasta.id.endswith('_2') or 'Brs' in fasta.id:
-                tag_type[fasta.id] = 'BRS'
-                to_add = re.search(fasta.id, r'a1,[0-9]')
-                if to_add != None:
-                    tag_type[fasta.id] += to_add
-            elif 'Asr' in fasta.id:
-                tag_type[fasta.id] = 'ASR'
+
+tag_type = {} #Dictionary to store gene types
+prot_list = [] #List to store protein information
+
+for infile in [infile1, infile2]: #Loop through input files
+    with open(infile) as handle: #Read input files
+        aa_seqs = SeqIO.parse(handle, 'fasta') #Parse file contents
+        for fasta in aa_seqs: #Loop through fasta records in the file
+            print(fasta.id) #Print the id of the fasta record
+            if ('Dsr' in fasta.id or 'falk' in fasta.id) and not fasta.id.endswith('_2'): #If the id includes certain annotations
+                tag_type[fasta.id] = 'DSR' #Assign the DRS (dextransucrase) gene type
+            elif fasta.id.endswith('_2') or 'Brs' in fasta.id: #Do the same for other gene types
+                tag_type[fasta.id] = 'BRS' #Branching sucrase
+                to_add = re.search(fasta.id, r'a1,[0-9]') #Search for the subtype of BRS
+                if to_add != None:  #If the subtype is indicated
+                    tag_type[fasta.id] += to_add #Add it to the BRS tag
+            elif 'Asr' in fasta.id: 
+                tag_type[fasta.id] = 'ASR' #Alternansucrase
             elif 'Rsr' in fasta.id:
-                tag_type[fasta.id] = 'RSR'
-            elif 'Msr' in fasta.id:
-                tag_type[fasta.id] = 'MSR'
+                tag_type[fasta.id] = 'RSR' #Reuteransucrase
+            elif 'Msr' in fasta.id: 
+                tag_type[fasta.id] = 'MSR' #Mutansucrase
             elif '4,6_gtf' in fasta.id:
-                tag_type[fasta.id] = 'GTFB_4,6'
+                tag_type[fasta.id] = 'GTFB_4,6' #4,6-glucanotransferase, GtfB-like
             elif '4,3_gtf' in fasta.id:
-                tag_type[fasta.id] = 'GTFB_4,3'
+                tag_type[fasta.id] = 'GTFB_4,3' #4,3-glucanotransferase, GtfB-like
             elif '_E' in fasta.id or '_P' in fasta.id:
-                tag_type[fasta.id] = 'GTFC'
-            else: tag_type[fasta.id] = 'ND'
-    
-    # for GH_type in GH_types:
-    #     with open(f'{infolder}/{GH_type}_codon.fna') as handle:
-    #         aa_seqs = SeqIO.parse(handle, 'fasta')
-    #         for fasta in aa_seqs:
-    #             tag_type[fasta.id] = GH_type
-    #         tag_type['A1003_12540'] = 'GS'
-    
-    # Loop through the sequences and retrieve the motifs
-    with open(infile) as handle:
-        aa_seqs = SeqIO.parse(handle, 'fasta')
-        for fasta in aa_seqs:
-            name, sequence = fasta.id, str(fasta.seq) #str(fasta.seq.translate())
-            category = tag_type[fasta.id]
-            motif_list = ['']
-            for i in range(1, 8):
-                motif_matches = search_motif(sequence, i)
-                motif_list.append(motif_matches)
-                for mot in motif_matches:
-                    mot = motif(*mot)
-                    if len(mot.seq) > 0:
-                        if len(motif_list[1]) > 1:
-                            category = 'DSR_BRS'
-                        with open(f'{outdir}/{output_file2}', 'a') as out_file2:
-                            out_file2.write(f'{name}\t{category}\t{mot.num}\t{mot.seq}\t{mot.start}\t{mot.end}\t{len(sequence)}\n')
-            prot = prot_sequence(name, motif_list[1], motif_list[2], motif_list[3], motif_list[4], motif_list[5], motif_list[6], motif_list[7])
-            prot.calculate_n_motifs()
-            prot_list.append(prot)
-            bool_list = list(map(int, prot.bool_list))
-            with open(f'{outdir}/{output_file}', 'a') as out_file:
+                tag_type[fasta.id] = 'GTFC' #GtfC-like
+            else: tag_type[fasta.id] = 'ND' #Not determined (unknown)
+
+            name, sequence = fasta.id, str(fasta.seq) #Get the name and sequence of the gene
+            category = tag_type[fasta.id] #Retrieve the gene category
+            motif_list = [''] #Create an empty motif list
+            for i in range(1, 8): #Loop through the motif numbers
+                motif_matches = search_motif(sequence, i) #Search for a motif in the protein
+                motif_list.append(motif_matches) #Add the match to the motif list
+                for mot in motif_matches: #Loop throuh motif information
+                    mot = motif(*mot) #Convert the motif into an object of class motif
+                    if len(mot.seq) > 0: #If the length of the motif is > 0
+                        if len(motif_list[1]) > 1: #If the length of the list with motif I is > 1
+                            category = 'DSR_BRS' #Change the category to DSR_BRS
+                        with open(f'{outdir}/{output_file2}', 'a') as out_file2: #Open the output file with motif sequence and position information
+                            out_file2.write(f'{name}\t{category}\t{mot.num}\t{mot.seq}\t{mot.start}\t{mot.end}\t{len(sequence)}\n') #Write the motif information into the file
+            
+            #Create an object of class protein_sequence with all the motifs and the ID of the fasta record as name
+            prot = prot_sequence(name, motif_list[1], motif_list[2], 
+                                 motif_list[3], motif_list[4], motif_list[5], 
+                                 motif_list[6], motif_list[7])
+            prot.calculate_n_motifs() #Get the motif presence/absence information
+            prot_list.append(prot) #Append the protein sequence to the list (only for debugging purposes)
+            
+            bool_list = list(map(int, prot.bool_list)) #Get the presence/absence list
+            with open(f'{outdir}/{output_file}', 'a') as out_file: #Open the output file with presence/absence information in append-only mode
                 out_file.write(f'{prot.locus}\t{category}\t{bool_list[1]}\t{bool_list[2]}\t'+
                                f'{bool_list[3]}\t{bool_list[4]}\t{bool_list[5]}\t'+
-                               f'{bool_list[6]}\t{bool_list[7]}\n')
+                               f'{bool_list[6]}\t{bool_list[7]}\n') #Write bool list as a row in the file
             
