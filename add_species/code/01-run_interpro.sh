7ef27086a81bf2ef8d6f2d658b40afca2d56bcd0
Marina <marina.motamerlo@icm.uu.se>
2024-11-12 18:04:53 +0100

    Annotate bash scripts

--- add_species/code/01-run_interpro.sh
+++ add_species/code/01-run_interpro.sh
@@ -1,10 +1,10 @@
-threads=16
-in_og="files/ingroups.faa"
-out_og="results/interproscan/GH70_species.tsv"
-annot_log="logs/01-interproscan.log"
+threads=16 #No. of threads to be used
+in_og="files/ingroups.faa" #Input fasta file with complete protein sequences
+out_og="results/interproscan/GH70_species.tsv" #Output file
+annot_log="logs/01-interproscan.log" #Log file
 
-mkdir -p $(dirname -- $annot_log)
-mkdir -p $(dirname -- $out_og)
+mkdir -p $(dirname -- $annot_log) #Create the log directory if it doesn't exist
+mkdir -p $(dirname -- $out_og) #Create the output directory if it doesn't exist
 
 # Get domain annotations of outgroups
 ~/interproscan-5.59-91.0/interproscan.sh -i $in_og -f tsv -o $out_og -dp -cpu $threads 2> $annot_log;
--- add_species/code/03-generate_trees.sh
+++ add_species/code/03-generate_trees.sh
@@ -1,25 +1,22 @@
-threads=16
-basedir=~/GH_project/add_species
-parentdir=~/GH_project
-in_og=$basedir"/results/domains/ingroup_domains.faa"
-in_GH70=$parentdir"/data/fasta/GH70/GH70_functional_repset.faa" #"files/GH70_30kb_small_subset.faa"  #"../data/fasta/GH70/GH70_functional_repset.faa"
-in_fasta=$basedir"/results/domains/GH70_species.faa"
-alignment=$basedir"/results/alignment/GH70_species.mafft.faa"
-aln_log=$basedir"/logs/03.1-alignment.log"
-tree_log=$basedir"/logs/03.2-tree.log"
+#OBS! This script requires to activate the Conda environment codeml-run
+threads=16 #No. of threads
+basedir=~/GH_project/add_species #Working directory
+parentdir=~/GH_project #Parent directory of the working directory
+in_og=$basedir"/results/domains/ingroup_domains.faa" #Input fasta file with GH70 domains from other species
+in_GH70=$parentdir"/data/fasta/GH70/GH70_functional_repset.faa" #Input fasta file with GH70 domains from the A. kunkeei representative dataset
+in_fasta=$basedir"/results/domains/GH70_species.faa" #Fasta file with both A. kunkeei domains and domains from other species
+alignment=$basedir"/results/alignment/GH70_species.mafft.faa" #Output alignment
+aln_log=$basedir"/logs/03.1-alignment.log" #Mafft log file
+tree_log=$basedir"/logs/03.2-tree.log" #IQtree log file
 
-mkdir -p $basedir/logs
-mkdir -p $(dirname -- $alignment)
+mkdir -p $basedir/logs #Create log directory if it doesn't exist
+mkdir -p $(dirname -- $alignment) #Create alignment directory if it doesn't exist
 
 # Put outgroups and GH70s from A. kunkeei in the same file
 cat $in_og $in_GH70 > $in_fasta
-
-# The following part required: conda activate codeml-run
 
 # Run alignment
 mafft-linsi --thread $threads $in_fasta > $alignment 2> $aln_log
 
 # Create tree
-iqtree --redo -nt $threads -s $alignment -st AA -mset Q.pfam,WAG,LG,JTT -bb 1000 -bnni > $tree_log 2> $tree_log
-
-# Former model -m LG+G4+F
+iqtree --redo -nt $threads -s $alignment -st AA -mset Q.pfam,WAG,LG,JTT -bb 1000 -bnni > $tree_log 2> $tree_log #Former model: -m LG+G4+F
