6aa5cd52ba3625bee3e85ad95121888e46a8c04e
Marina <marina.motamerlo@icm.uu.se>
2025-01-30 17:33:58 +0100

    Add scripts to generate a phylogeny of a subset of strains

---
+++ code/concatenate_core_subset.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 12:49:25 2025
+
+Python script to make a strain phylogeny based on core genes for any set of
+strains.
+
+@author: Marina Mota-Merlo
+"""
+
+from Bio import SeqIO
+import os, subprocess
+# from Bio.Nexus import Nexus
+
+inpath = os.path.expanduser('~') + '/GH_project/all_core/38_strains/fasta'
+# outpath = inpath.replace('fasta', 'nexus')
+# nexus_out = f'{outpath}/concatenated.nex'
+fasta_out = f'{inpath}/concatenated.faa'
+log = os.path.expanduser('~') + '/GH_project/logs/iqtree/38_strains_core_iqtree.log'
+outdir = inpath.replace('fasta', 'trees')
+aln_dir = inpath.replace('fasta', 'alignment')
+threads = 48
+
+if not os.path.exists(aln_dir):
+    os.makedirs(aln_dir)
+
+infiles = [f'{inpath}/{file}' for file in os.listdir(inpath) if file.endswith('.mafft.faa')]
+
+for file in infiles:
+    outfile = file.replace(inpath, aln_dir)
+    records = []
+    with open(file) as handle:
+        for record in SeqIO.parse(handle, 'fasta'):
+            record.id = record.id.split('_')[0].replace('K2W83', 'DSM').replace('LDX55', 'IBH001').replace('FHON', 'fhon')
+            if record.id.startswith('H'):
+                record.id = record.id[:4] + '-' + record.id[4:]
+            record.description = record.id
+            records.append(record)
+            print(record.id)
+    with open(outfile, 'w') as out_file:
+        SeqIO.write(records, out_file, 'fasta')
+            
+subprocess.run(f'iqtree2 -nt AUTO -ntmax {threads} -p {aln_dir} --prefix 38strains -st AA -mset Q.pfam,LG,WAG,JTT -bb 1000 -bnni > {log}', shell = True)
+subprocess.run(f'mkdir -p {outdir}', shell = True)
+subprocess.run(f'mv 38strains.* {outdir}', shell = True)
+
+# import pandas as pd
+
+# def fasta_reader(file):
+#     fasta_df = pd.read_csv(file, sep='>', lineterminator='>', header=None)
+#     fasta_df[['Accession', 'Sequence']] = fasta_df[0].str.split('\n', 1, \
+#                                         expand=True)
+#     new_acc = fasta_df['Accession'].to_list()
+#     new_accs = [acc.split(' ')[0].split('_')[0].replace('K2W83', 'DSM').replace('LDX55', 'IBH001').replace('FHON', 'fhon') for acc in new_acc]
+#     new_accs = [acc[:4] + '-' + acc[4:] if acc.startswith('H') else acc for acc in new_accs]
+#     fasta_df['Accession'] = new_accs
+#     fasta_df.drop(0, axis=1, inplace=True)
+#     fasta_df['Sequence'] = fasta_df['Sequence'].replace('\n', '', regex=True)
+#     return fasta_df
+
+# df = pd.concat(fasta_reader(i) for i in infiles)
+# agg_func = {'Sequence': 'sum'}
+# df_new = df.groupby(df['Accession']).aggregate(agg_func)
+
+# acc = [f'>{strain}' for strain in list(df_new.index)]
+# df_new = pd.concat([pd.Series(acc, index=df_new.index, name='Accession'), df_new], axis=1)
+
+# df_new.to_csv(fasta_out, sep='\n', index=None, header=None)
+
+# subprocess.run(f'iqtree -nt AUTO -ntmax {threads} -s {fasta_out} -st AA -mset Q.pfam,LG,WAG,JTT -bb 1000 -bnni > {log}', shell = True)
+# subprocess.run(f'mkdir -p {outdir}', shell = True)
+# subprocess.run(f'mv {fasta_out}.* {outdir}', shell = True)
---
+++ code/get_core_subset.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 16:41:49 2025
+
+Python script to get the core genes for a subset of 38 strains and produce
+an alignment of the amino acid sequences.
+
+Requires Biopython and Mafft.
+
+@author: Marina Mota-Merlo
+"""
+
+# =============================================================================
+# 0. Import required packages
+# =============================================================================
+import os
+from Bio import SeqIO
+import subprocess
+
+# =============================================================================
+# 1. Define inputs and outputs, create paths
+# =============================================================================
+outpath = os.path.expanduser('~') + '/GH_project/all_core/38_strains/fasta' #Path to save outputs
+
+representatives = ['A0901', 'A1001', 'A1003', 'A1202', 'A1401', 'A1404', 
+                   'A1805', 'K2W83', 'fhon2', 'G0101', 'G0403', 'H1B1-04J', 
+                   'H1B1-05A', 'H1B3-02M', 'H3B1-01A', 'H3B1-04J', 'H3B1-04X', 
+                   'H3B2-02X', 'H3B2-03J', 'H3B2-03M', 'H3B2-06M', 'H3B2-09X', 
+                   'H4B1-11J', 'H4B2-02J', 'H4B2-04J', 'H4B2-05J', 'H4B2-06J', 
+                   'H4B2-11M', 'H4B4-02J', 'H4B4-05J', 'H4B4-06M', 'H4B4-12M',
+                   'H4B5-01J', 'H4B5-03X', 'H4B5-04J', 'H4B5-05J', 'LDX55',
+                   'MP2'] #Set of strains of interest
+
+inpath = os.path.expanduser('~') + '/GH_project/all_core/fasta' #Path to input files
+
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+    
+faas = [file for file in os.listdir(inpath) if file.endswith('faa') and 'mafft' not in file] #Raw fasta files with the protein sequences of core genes (including all strains)
+fnas = [file.replace('faa', 'fna') for file in faas] #Same, but with nucleotide sequences
+
+# =============================================================================
+# 2. Create fasta files with the nucleotide and amino acid sequences of core
+# genes
+# =============================================================================
+for file in faas: #Loop through protein fasta
+    with open(f'{inpath}/{file}') as handle: #Open input file
+        records = [] #Create an empty list to store the records
+        for record in SeqIO.parse(handle, 'fasta'): #Loop through the records in the input file
+            if any([repre.replace('-', '').upper() in record.id.upper() for repre in representatives]): #If the records are in representative strains
+                print(record.id) #Print the record
+                records.append(record) #Add the record to the list
+        with open(f'{outpath}/{file}', 'w') as out_file: #Open the output file
+            SeqIO.write(records, out_file, 'fasta') #Write the records to the output file
+                    
+    with open(f'{inpath}/{file.replace("faa", "fna")}') as handle_fna: #Same as above, but for nucleotide sequences
+        records_fna = []
+        for record in SeqIO.parse(handle_fna, 'fasta'):
+            if any([repre.replace('-', '').upper() in record.id.upper() for repre in representatives]):
+                print(record.id)
+                records_fna.append(record)
+                with open(f'{outpath}/{file.replace("faa", "fna")}', 'w') as out_file:
+                    SeqIO.write(records_fna, out_file, 'fasta')
+                
+# =============================================================================
+# 3. Create alignments of the amino acid fasta files
+# =============================================================================
+for file in os.listdir(outpath): #Loop through the files generated in the previous step
+    if file.endswith('.faa') and 'mafft' not in file: #Check that the input file is not an alignment
+        out_aln = file.replace('.faa', '.mafft.faa') #Set the name of the output file
+        subprocess.run(f'mafft-linsi --thread 12 {outpath}/{file} > {outpath}/{out_aln}', 
+                       shell = True) #Create the alignments using 12 threads
